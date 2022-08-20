aec0f29479cc0f1d53e6647d20c6988d9b3348ba
northmaster <devnorth@protonmail.com>
2022-08-20 00:44:21 -0300

    feat: save and load from file function

--- deck/deck.go
+++ deck/deck.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"fmt"
+	"strings"
+	"io/ioutil"
+	"os"
 )
 
 type deck []string
 
+// generates a deck
 func newDeck() deck {
 	cards := deck{}
 
@@ -21,12 +25,35 @@ func newDeck() deck {
 	return cards
 }
 
+// print deck contents
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
 }
 
+// split deck in two
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+// convert deck to string
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+// save current deck to a text file
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+// load deck from text file
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	s := strings.Split(string(bs), ",")
+	return deck(s)
 }
